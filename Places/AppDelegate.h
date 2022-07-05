0b06d918da5d46af2354389ed40c5b2cdb111f3e
Iris Fu <irisfu@fb.com>
2022-07-05 15:50:18 -0700

    Initial Commit

---
+++ Places/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Places
+//
+//  Created by Iris Fu on 7/5/22.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (readonly, strong) NSPersistentCloudKitContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
