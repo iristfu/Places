52d2af3eefa8e071b4da578683b8fd80b81dedba
Iris Fu <irisfu@fb.com>
2022-07-12 15:30:45 -0700

    Now incrementing favorites count for places that gets favorited by a user but hasn't previously been favorited

--- Places/Views/PlaceTableViewCell.m
+++ Places/Views/PlaceTableViewCell.m
@@ -22,20 +22,39 @@
     // Configure the view for the selected state
 }
 
+- (bool)notFavoritedBy:(PFUser *)currentUser {
+    return ![currentUser[@"favoritedPlaces"] containsObject:self.place[@"place_id"]];
+}
+
 - (IBAction)didTapAddToFavorites:(id)sender {
-    NSLog(@"didTapAddToFavorites called");
+    PFUser *currentUser = [PFUser currentUser];
+    
     
     // Increase the Places’s favorite count in the Parse Place model
-//    if (
-//    Place *placeToFavorite = [Place new];
-//    placeToFavorite.placeID = self.place[@"place_id"];
-    
-    // Add the place to the the current User's favorites list
-    PFUser *currentUser = [PFUser currentUser];
-    if (![currentUser[@"favoritedPlaces"] containsObject:self.place[@"place_id"]]) {
-        [currentUser addObject:self.place[@"place_id"] forKey:@"favoritedPlaces"];
-        [currentUser saveInBackground];
-    }
-    NSLog(@"The user's favoritedPlaces array is now: %@", currentUser[@"favoritedPlaces"]);
+    PFQuery *query = [PFQuery queryWithClassName:@"Place"];
+    [query whereKey:@"placeID" equalTo:self.place[@"place_id"]];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *places, NSError *error) {
+        if (error) {
+            NSLog(@"Got error getting place in didTapAddToFavorites");
+        } else {
+            if ([places count] != 1) {
+                NSLog(@"Could not find place for place_id to increment favoriteCount");
+            } else {
+                if ([self notFavoritedBy:currentUser]) {
+                    NSLog(@"Place not favorited by current user");
+                    [currentUser addObject:self.place[@"place_id"] forKey:@"favoritedPlaces"];
+                    [currentUser saveInBackground];
+                    NSLog(@"The user's favoritedPlaces array is now: %@", currentUser[@"favoritedPlaces"]);
+                    
+                    [places[0] incrementKey:@"favoriteCount"];
+                    [places[0] saveInBackground];
+                    NSLog(@"Incremented favorite count for %@", places[0][@"name"]);
+                } else {
+                    NSLog(@"%@ already favorited by user", places[0][@"name"]);
+                }
+            }
+        }
+    }];
 }
+
 @end
