5c986ede2ccaf15739987f0e0aed422b9ddc129d
Iris Fu <irisfu@fb.com>
2022-07-14 23:40:27 -0700

    Created UI of compose view controller

---
+++ Places/View Controllers/ComposeItineraryViewController.h
@@ -0,0 +1,16 @@
+//
+//  ComposeItineraryViewController.h
+//  Places
+//
+//  Created by Iris Fu on 7/14/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeItineraryViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
