3cb00c9e28d638254db2370dec060e492d3da67b
Iris Fu <irisfu@fb.com>
2022-07-15 12:01:12 -0700

    Finally debugged delegate... forgot to set segue identifier :/

--- Places/View Controllers/ItinerariesTableViewController.m
+++ Places/View Controllers/ItinerariesTableViewController.m
@@ -82,17 +82,12 @@
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-    
     if ([[segue identifier] isEqualToString:@"ComposeItinerarySegue"]) {
-        NSLog(@"in compose itinerary prepare for segue");
+        NSLog(@"Preparing for ComposeItinerarySegue");
         UINavigationController *navigationController = [segue destinationViewController];
         ComposeItineraryViewController *composeItineraryViewController = (ComposeItineraryViewController *)navigationController.topViewController;
         composeItineraryViewController.delegate = self;
-        NSLog(@"The compose view controller's delegate is %@", composeItineraryViewController.delegate);
     }
 }
-
 
 @end
