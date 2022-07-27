7d8bb75de5795e2952e827322a9968dd1d9a7554
Iris Fu <irisfu@fb.com>
2022-07-27 10:52:34 -0700

    nullable

--- Places/View Controllers/ShortestRouteMapViewController.h
+++ Places/View Controllers/ShortestRouteMapViewController.h
@@ -12,7 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ShortestRouteMapViewController : UIViewController
 
-@property (strong, nonatomic) Itinerary *itinerary;
+@property (strong, nonatomic, nullable) Itinerary *itinerary;
 
 @end
 
