6dd2db38afc692ca2e8c0a168bf1a42feac6b6d2
Iris Fu <irisfu@fb.com>
2022-08-03 12:17:48 -0700

    Add share itinerary view controller

---
+++ Places/View Controllers/ShareItineraryViewController.h
@@ -0,0 +1,16 @@
+//
+//  ShareItineraryViewController.h
+//  Places
+//
+//  Created by Iris Fu on 8/3/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ShareItineraryViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Places/View Controllers/ShareItineraryViewController.m
@@ -0,0 +1,31 @@
+//
+//  ShareItineraryViewController.m
+//  Places
+//
+//  Created by Iris Fu on 8/3/22.
+//
+
+#import "ShareItineraryViewController.h"
+
+@interface ShareItineraryViewController ()
+
+@end
+
+@implementation ShareItineraryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
