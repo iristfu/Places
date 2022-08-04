b8da9fda8c1b58e576b4930ff3e40fd707774885
Iris Fu <irisfu@fb.com>
2022-08-04 12:08:34 -0700

    username share cell ui autolayout

---
+++ Places/Views/ShareWithUsernameCell.h
@@ -0,0 +1,16 @@
+//
+//  ShareWithUsernameCell.h
+//  Places
+//
+//  Created by Iris Fu on 8/4/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ShareWithUsernameCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Places/Views/ShareWithUsernameCell.m
@@ -0,0 +1,23 @@
+//
+//  ShareWithUsernameCell.m
+//  Places
+//
+//  Created by Iris Fu on 8/4/22.
+//
+
+#import "ShareWithUsernameCell.h"
+
+@implementation ShareWithUsernameCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
