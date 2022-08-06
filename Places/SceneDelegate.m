dde80a6bcf5fb8c93118df138cae55d997459424
Iris Fu <irisfu@fb.com>
2022-08-05 17:17:30 -0700

    clean up code

--- Places/SceneDelegate.m
+++ Places/SceneDelegate.m
@@ -45,32 +45,6 @@
     // In future PR: may need to handle custom URL here for when the app isn't launched and the user clicks a custom URL
 }
 
-- (NSArray<NSString *> *)getCurrentSharedItinerariesObjectIDs {
-    NSMutableArray<NSString *> *objectIDs = [[NSMutableArray alloc] init];
-    PFUser *currentUser = [PFUser currentUser];
-    for (Itinerary *itinerary in currentUser[@"sharedItineraries"]) {
-        [objectIDs addObject:itinerary.objectId];
-    }
-    return [objectIDs copy];
-}
-
-- (void)addSharedItineraryForCurrentUser:(Itinerary *)newItinerary
-                    withAccessPermission:(NSString *)accessPermission {
-    PFUser *currentUser = [PFUser currentUser];
-    NSArray<NSString *> *currentSharedItinerariesObjectIDS = [self getCurrentSharedItinerariesObjectIDs];
-    if (![currentSharedItinerariesObjectIDS containsObject:newItinerary.objectId]) {
-        [currentUser addObject:newItinerary forKey:@"sharedItineraries"];
-        [currentUser saveInBackground];
-    } else {
-        NSLog(@"Already stored shared itinerary");
-    }
-    
-    // store user's access permission to shared itinerary
-    if ([accessPermission isEqualToString:@"view"]) {
-        [currentUser addObject:newItinerary.objectId forKey:@"viewOnlyItineraryIDs"];
-        [currentUser saveInBackground];
-    }
-}
 
 - (void)scene:(UIScene *)scene openURLContexts:(NSSet<UIOpenURLContext *> *)URLContexts {
     NSLog(@"This should be called if app opens a URL while running or suspended in memory");
@@ -91,7 +65,6 @@
     PFQuery *query = [PFQuery queryWithClassName:@"Itinerary"];
     [query getObjectInBackgroundWithId:itineraryObjectID block:^(PFObject *itinerary, NSError *error) {
         if (!error) {
-//            [self addSharedItineraryForCurrentUser:itinerary withAccessPermission:access];
             PFUser *currentUser = [PFUser currentUser];
             [access isEqualToString:@"view"] ? [itinerary addObject:currentUser forKey:@"usersWithViewAccess"] : [itinerary addObject:currentUser forKey:@"usersWithEditAccess"];
             [itinerary saveInBackground];
