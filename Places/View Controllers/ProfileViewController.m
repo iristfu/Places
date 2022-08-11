803333237a2f024fe23550accd6e0c3dbad22270
Iris Fu <irisfu@fb.com>
2022-08-11 15:00:30 -0700

    polished profile, can edit profile picture

--- Places/View Controllers/ProfileViewController.m
+++ Places/View Controllers/ProfileViewController.m
@@ -16,6 +16,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *username;
 @property (weak, nonatomic) IBOutlet UILabel *bio;
 @property (weak, nonatomic) IBOutlet PFImageView *profilePicture;
+@property (strong, nonatomic) IBOutlet UIButton *editProfilePictureButton;
+- (IBAction)didTapEditProfilePictureButton:(id)sender;
 
 
 @end
@@ -42,6 +44,15 @@
         [self.profilePicture setImage:placeHolderImage];
         NSLog(@"Set placeholder image %@", placeHolderImage);
     }
+    self.profilePicture.layer.cornerRadius = self.profilePicture.frame.size.height / 2;
+    self.profilePicture.layer.masksToBounds = YES;
+    self.profilePicture.layer.borderWidth = 0;
+    self.profilePicture.contentMode = UIViewContentModeScaleAspectFill;
+    
+//    self.editProfilePictureButton.layer.cornerRadius = self.editProfilePictureButton.frame.size.width / 2;
+//    self.editProfilePictureButton.layer.masksToBounds = YES;
+//    self.editProfilePictureButton.layer.borderWidth = 0;
+//    self.editProfilePictureButton.contentMode = UIViewContentModeScaleAspectFill;
     
     if (currentUser[@"bio"]) {
         self.bio.text = currentUser[@"bio"];
@@ -63,5 +74,72 @@
     ProfileViewController *profileViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
     myDelegate.window.rootViewController = profileViewController;
 }
+
+- (IBAction)didTapEditProfilePictureButton:(id)sender {
+    [self renderImagePicker];
+}
+
+-(void)renderImagePicker {
+    // set up image picker
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = true;
+    imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    if (editedImage) {
+        [self.profilePicture setImage:editedImage];
+    } else {
+        [self.profilePicture setImage:originalImage];
+    }
+    
+    PFUser *currentUser = [PFUser currentUser];
+    if (currentUser) {
+        CGSize size = CGSizeMake(300, 300);
+        UIImage *uploadImage = [self resizeImage:self.profilePicture.image withSize:size];
+        currentUser[@"profilePicture"] = [self getPFFileFromImage:uploadImage];
+        [currentUser saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+            if(succeeded) {
+                [self dismissViewControllerAnimated:YES completion:nil];
+            } else {
+                NSLog(@"Error changing profile picture");
+            }
+        }];
+    }
+}
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+- (PFFileObject *)getPFFileFromImage: (UIImage * _Nullable)image {
+    // check if image is not nil
+    if (!image) {
+        return nil;
+    }
+    NSData *imageData = UIImagePNGRepresentation(image);
+    // get image data and check if that is not nil
+    if (!imageData) {
+        return nil;
+    }
+    return [PFFileObject fileObjectWithName:@"image.png" data:imageData];
+}
+
 
 @end
